a6a30ad7a3cd9fa3e64a0324b27901b3952efd98
fmshot2 <femishotolaa@gmail.com>
2023-01-02 05:40:53 +0100

    first commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+
+            //Admin
+            [
+                'full_name' => 'Femi Admin',
+                'username' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('1111'),
+                'role' => 'admin',
+                'status' => 'active',
+            ],
+
+            //Vendor
+            [
+                'full_name' => 'Femi Vendor',
+                'username' => 'Vendor',
+                'email' => 'vendor@gmail.com',
+                'password' => Hash::make('1111'),
+                'role' => 'vendor',
+                'status' => 'active',
+            ],
+
+            //Customer
+            [
+                'full_name' => 'Femi Customer',
+                'username' => 'Customer',
+                'email' => 'customer@gmail.com',
+                'password' => Hash::make('1111'),
+                'role' => 'customer',
+                'status' => 'active',
+            ]
+        ]);
+    }
+}
