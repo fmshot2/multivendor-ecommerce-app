493ab8de7d8d68ca9ed78397c4ccd7b129d75755
fmshot2 <femishotolaa@gmail.com>
2023-01-16 10:04:53 +0100

    BANNER STARTED
    BANNER STRTED

---
+++ resources/views/backend/banners/create.blade.php
@@ -0,0 +1,166 @@
+@extends('backend.layouts.master')
+
+@section('content')
+
+<div id="main-content">
+    <div class="container-fluid">
+        <div class="block-header">
+            <div class="row">
+                <div class="col-lg-6 col-md-8 col-sm-12">
+                    <h2><a href="javascript:void(0);" class="btn btn-xs btn-link btn-toggle-fullwidth"><i class="fa fa-arrow-left"></i></a>Add Banners</h2>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{route('admin')}}"><i class="icon-home"></i></a></li>
+                        <li class="breadcrumb-item">Banners</li>
+                        <li class="breadcrumb-item active">Add Banners</li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 col-md-4 col-sm-12 text-right">
+                    <div class="inlineblock text-center m-r-15 m-l-15 hidden-sm">
+                        <div class="sparkline text-left" data-type="line" data-width="8em" data-height="20px" data-line-Width="1" data-line-Color="#00c5dc"
+                            data-fill-Color="transparent">3,5,1,6,5,4,8,3</div>
+                        <span>Visitors</span>
+                    </div>
+                    <div class="inlineblock text-center m-r-15 m-l-15 hidden-sm">
+                        <div class="sparkline text-left" data-type="line" data-width="8em" data-height="20px" data-line-Width="1" data-line-Color="#f4516c"
+                            data-fill-Color="transparent">4,6,3,2,5,6,5,4</div>
+                        <span>Visits</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <div class="card">
+                    <div class="header">
+                        <h2><strong>Basic</strong> Information <small>Description text here...</small> </h2>
+                        <ul class="header-dropdown">
+                            <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></a>
+                                <ul class="dropdown-menu dropdown-menu-right slideUp">
+                                    <li><a href="javascript:void(0);" class="waves-effect waves-block">Action</a></li>
+                                    <li><a href="javascript:void(0);" class="waves-effect waves-block">Another action</a></li>
+                                    <li><a href="javascript:void(0);" class="waves-effect waves-block">Something else</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="body">
+                        <div class="row clearfix">
+                            <div class="col-lg-6 col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="First Name">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Last Name">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row clearfix">
+                            <div class="col-lg-4 col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Phone No.">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <input data-provide="datepicker" data-date-autoclose="true" class="form-control" placeholder="Date of Birth">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Age">
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12">
+                                <select class="form-control show-tick">
+                                    <option value="">-- Gender --</option>
+                                    <option value="10">Male</option>
+                                    <option value="20">Female</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12">
+                                <select class="form-control show-tick">
+                                    <option value="">-- Department --</option>
+                                    <option value="10">BCA</option>
+                                    <option value="20">MCA</option>
+                                    <option value="20">BCom</option>
+                                    <option value="20">MCom</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4 col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Enter Your Email">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row clearfix">
+                            <div class="col-sm-12">
+                                <input type="file" class="dropify">
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="form-group mt-3">
+                                    <textarea rows="4" class="form-control no-resize" placeholder="Description"></textarea>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-outline-secondary">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row clearfix">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="header">
+                        <h2><strong>Account</strong> Information <small>Description text here...</small> </h2>
+                        <ul class="header-dropdown">
+                            <li class="dropdown"> <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"></a>
+                                <ul class="dropdown-menu dropdown-menu-right slideUp">
+                                    <li><a href="javascript:void(0);" class="waves-effect waves-block">Action</a></li>
+                                    <li><a href="javascript:void(0);" class="waves-effect waves-block">Another action</a></li>
+                                    <li><a href="javascript:void(0);" class="waves-effect waves-block">Something else</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="body">
+                        <div class="row clearfix">
+                            <div class="col-lg-6 col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Phone">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Password">
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-12">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Confirm Password">
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" class="btn btn-outline-secondary">Cancel</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+@endsection
---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- Javascript -->
+<script src="{{asset('backend/assets/bundles/libscripts.bundle.js')}}"></script>
+<script src="{{asset('backend/assets/bundles/vendorscripts.bundle.js')}}"></script>
+
+<script src="{{asset('backend/assets/bundles/jvectormap.bundle.js')}}"></script> <!-- JVectorMap Plugin Js -->
+<script src="{{asset('backend/assets/bundles/morrisscripts.bundle.js')}}"></script>
+<script src="{{asset('backend/assets/bundles/knob.bundle.js')}}"></script>
+<script src="{{asset('backend/assets/bundles/mainscripts.bundle.js')}}"></script>
+<script src="{{asset('backend/assets/js/pages/ui/sortable-nestable.js')}}"></script>
+<!-- Jquery DataTable Plugin Js -->
+<script src="{{asset('backend/assets/bundles/datatablescripts.bundle.js')}}"></script>
+<script src="{{asset('backend/assets/js/pages/tables/jquery-datatable.js')}}"></script>
+<script src="{{asset('backend/assets/js/index.js')}}"></script>
